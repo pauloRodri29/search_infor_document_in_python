1d71ca610764cdbfe07e45040a0c2b1c0207aab5
PauloRodri29 <rodriguesjp29@gmail.com>
2025-01-09 21:56:15 -0300

    Função de extração de dados melhorada: adcionado paramentros

--- utils/extraindo.py
+++ utils/extraindo.py
@@ -14,10 +14,10 @@ def extract_info_from_text(patterns, text):
     while True:
         # Flag para verificar se todos os valores são None
         all_none = True
-        
+
         for key, regex in patterns.items():
             match = re.findall(regex, text, flags=re.IGNORECASE | re.DOTALL)  # Busca o padrão no texto
-            
+
             if match:
                 # Se match for uma lista de tuplas (com grupos de captura), a primeira tupla é a que você quer
                 if isinstance(match[0], tuple):
@@ -32,7 +32,7 @@ def extract_info_from_text(patterns, text):
                         client_info[key] = match[client_number - 1].strip()  # Ajuste para acessar o item correto
                     else:
                         client_info[key] = None  # Se não houver valor suficiente
-            
+
             else:
                 client_info[key] = None  # Caso não encontre, registra como None
 
@@ -43,7 +43,7 @@ def extract_info_from_text(patterns, text):
         # Se todos os valores forem None, interrompe o loop
         if all_none:
             break
-        
+
         # Após processar todas as chaves, adiciona o client_info à lista de resultados
         if any(value is not None for value in client_info.values()):  # Garante que ao menos um valor seja encontrado
             results.append(client_info)
@@ -51,7 +51,7 @@ def extract_info_from_text(patterns, text):
             client_info = {}  # Limpa o dicionário para o próximo cliente
         else:
             break
-    
+
     return results
 
 
@@ -90,9 +90,9 @@ def extract_references_from_pdfs(input_files, references_dict, pages_to_extract=
                         for value in found_values:
                             value["Página-Arquivo"] = f" P{page_number} - A{count_file}"  # Adiciona o número da página
                             extracted_data.append(value)  # Adiciona cada cliente encontrado à lista
-                
+
                 count_file += 1
-                
+
             else:
                 logging.info(f"Arquivo {file} não suportado..")
 
@@ -114,7 +114,7 @@ def create_table(extracted_data, references_dict):
         row = {}  # Cria um dicionário para a linha
         # Adiciona o nome da página se disponível
         row["Página-Arquivo"] = page_data.get("Página-Arquivo", "N/A")
-        
+
         # Itera sobre o dicionário de referências
         for key in references_dict.keys():
             value = page_data.get(key, "N/A")
@@ -122,10 +122,10 @@ def create_table(extracted_data, references_dict):
                 row[key] = ", ".join(value)
             else:
                 row[key] = value
-        
+
         # Adiciona o dicionário formatado à lista de dados
         table_data.append(row)
-    
+
     # Converte os dados em um DataFrame do pandas
     df = pd.DataFrame(table_data)
     return df
@@ -133,137 +133,34 @@ def create_table(extracted_data, references_dict):
 def save_table_to_file(dataframe, output_file):
     try:
         # Salva o DataFrame em um arquivo Excel
-        dataframe.to_excel(output_file, index=False, sheet_name="Base de dados")
-        logging.info(f"Dados salvos com sucesso no arquivo Excel: {output_file}")
+        return dataframe.to_excel(output_file, index=False, sheet_name="Base de dados")
+        # logging.info(f"Dados salvos com sucesso no arquivo Excel: {output_file}")
     except Exception as e:
         logging.error(f"Erro ao salvar o arquivo Excel: {e}")
 
 # Função principal
-def main(list_input_files=None, references_dict=None, name_output_file="ArquivosExtraidos.xlsx",pages_to_extract=None, max_pages=None, start_page=0):
-    # input_files = ["fatura.pdf"]
-    # input_files = ["fatura.pdf"]
-    
-    # Dicionário com as referências e expressões regulares
-    # references_dict = {
-    #     # "Conta Contrato": r"CONTA CONTRATO:\s*(\d+)",
-    #     # "Hash_Code": r"Hash Code:\s+([\w\d\.]+)",
-    #     # "Documento": r"Documento:\s+(\d+)",
-    #     # "Empresa": r"Empresa:\s+([^\n]+)",
-    #     # "Municipio": r"Município:\s+([\w\s]+?)(?=\s+Bairro|$)",
-    #     # "Vencimento": r"Vencimento:\s*(\d{2}\-\d{2}\-\d{4})",
-    #     # "Valor_total": r"Valor:\s+([\d,\.]+)",
-        
-    #     "Instalacao": r"Instalação:\s+(\d+)",
-    #     "Endereço, N°": r"Endereço:\s+((?!STO ANTONIO , 0)[^\n]+)(?=\s+Bairro|$)",
-    #     "Bairro": r"Bairro:\s+([A-Za-z\s]+)(?=\s+Instalação|$)",
-    #     "Complemento": r"Complemento:\s+([^\n]+)(?=\s+Fatura|$)",
-    #     "N° Fatura": r"Fatura:\s+(\d+)",
-    #     "Classe Principal": r"Classe Principal\s*[^0-9]*\s*(\d+)",
-    #     "Classe de Consumo": r"Classe de Consumo(?:.*?\n)*?.*?(?:\d+\n)(\d+)",
-    #     "Tensão": r"(?<=\bTensão\b)(?:\D*\d+){2}\s*(\S+)",
-    #     "Fase": r"(?<=\bFase\b)(?:\D*\d+){3}\D*(\S+)",
-    #     "Data Fatura": r"(?<=\bData Fat\b)(?:\D*\d+){5}\D*(\S+)",
-    #     "Dias Fatura": r"(?<=\bDias Fat\b)(?:\D*\d+){8}\D*(\S+)",
-    #     "Data Leitura Anterior": r"(?<=\bDta\.Leit\.Ant\b|\bDat\.Leit\.Ant\b)(?:\D*\d+){9}\D*(\S+)",
-    #     "Data Leitura Atual": r"(?<=\bDta\.Leit\.Ant\b|\bDat\.Leit\.Ant\b)(?:\D*\d+){12}\D*(\S+)",
-        
-    #     # "Reaviso": r"(?<=\bReaviso\b)",
-    #     # "Corte": r"(?<=\bDta\.Leit\.Ant\b|\bDat\.Leit\.Ant\b)(?:\D*\d+){12}\D*(\S+)",
-        
-    #     "V.T.: Icms(BaseCalculo)": r"ICMS\s+(?:\D*\d+.+?\d+\n){0}([\d,\.]+)",
-    #     "V.T.: Icms(Aliquota)": r"ICMS\s+(?:\D*\d+.+?\d+\n){1}([\d,\.]+)",
-    #     "V.T.: Icms(Valor)": r"ICMS\s+(?:\D*\d+.+?\d+\n){2}([\d,\.]+)",
-    #     "V.T.: Cofins": r"COFINS\s+(?:\D*\d+.+?\d+\n){0}([\d,\.]+)",
-    #     "V.T.: Cofins(Aliquota)": r"COFINS\s+(?:\D*\d+.+?\d+\n){1}([\d,\.]+)",
-    #     "V.T.: Cofins(Valor)": r"COFINS\s+(?:\D*\d+.+?\d+\n){2}([\d,\.]+)",
-    #     "V.T.: Pis": r"PIS\s+(?:\D*\d+.+?\d+\n){0}([\d,\.]+)",
-    #     "V.T.: Pis(Aliquota)": r"PIS\s+(?:\D*\d+.+?\d+\n){1}([\d,\.]+)",
-    #     "V.T.: Pis(Valor)": r"PIS\s+(?:\D*\d+.+?\d+\n){2}([\d,\.]+)",
-        
-    #     # "V.M.: Esp": r"(\bCAT\b)([A-Z])?\b",
-    #     "V.M.: Medidor": r"CAT\s+(?:\D*\d+.+?\d+\n){0}([\d,\.]+)",
-    #     # "V.M.: CTE": r"CAT\s+(?:\D*\d+.+?\d+\n){1}([\d,\.]+)",
-    #     # "V.M.: FP": r"CAT\s+(?:\D*\d+.+?\d+\n){2}([\d,\.]+)",
-    #     # "V.M.: Leitura_Anterior": r"CAT\s+(?:\D*\d+.+?\d+\n){3}([\d,\.]+)",
-    #     # "V.M.: Leitura_Atual": r"CAT\s+(?:\D*\d+.+?\d+\n){4}([\d,\.]+)",
-    #     # "V.M.: Medido": r"CAT\s+(?:\D*\d+.+?\d+\n){5}([\d,\.]+)",
-    #     # "V.M.: Faturado": r"CAT\s+(?:\D*\d+.+?\d+\n){6}([\d,\.]+)",
-        
-    #     # "V.F.: Consumo(Quantidade)": r"Consumo\s+(?:)([\d.,]+)[\s\S]*?",
-    #     # "V.F.: Consumo(Preço)": r"Consumo\s+(?:)([\d.,]+)[\s\S]*?",
-    #     # "V.F.: Consumo(Valor)": r"Consumo\s+(?:)([\d.,]+)[\s\S]*?",
-    #     # "Cip-Ilum Pub Pref Munic (Valor)":r"Cip-Ilum Pub Pref Munic\s+([\d\.,]+)",
-    #     # "V.F.: Adcional Bandeira (Valor)": r"Adicional Bandeira\s+([\d\.,]+)",
-    #     # "V.F.: Crédito Prazo Atendimento (Valor)": r"Crédito Prazo Atendimento\s+([\d\.,]+)",
-    #     # "V.F.: Tributo a Reter IRPJ (Valor) ": r"Tributo a Reter IRPJ\s+([\d\.,]+)",
-    #     # "V.F.: Saldo em Aberto (Valor)": r"Saldo em aberto\s+([\d\.,]+)",
-    # }
-    # Adicionar uma coluna para contagem de páginas
-    # headers = ["Referência"] + list(references_dict.keys())
-    
-    # # # Extrair os valores do arquivo
-    extracted_data = extract_references_from_pdfs(list_input_files, references_dict, start_page=start_page, max_pages=max_pages, pages_to_extract=pages_to_extract)
-    # logging.info(extracted_data)
-    
-    # # Preparar os dados para exibição em tabela
-    df = create_table(extracted_data, references_dict)
+import logging
 
-    # # Salvar os dados no Excel
-    save_table_to_file(df, name_output_file)
-    # logging.info(text)
-    
-# if __name__ == "__main__":
-#     main(list_input_files=["fatura.pdf"], references_dict={
-#         # "Conta Contrato": r"CONTA CONTRATO:\s*(\d+)",
-#         # "Hash_Code": r"Hash Code:\s+([\w\d\.]+)",
-#         # "Documento": r"Documento:\s+(\d+)",
-#         # "Empresa": r"Empresa:\s+([^\n]+)",
-#         # "Municipio": r"Município:\s+([\w\s]+?)(?=\s+Bairro|$)",
-#         # "Vencimento": r"Vencimento:\s*(\d{2}\-\d{2}\-\d{4})",
-#         # "Valor_total": r"Valor:\s+([\d,\.]+)",
-        
-#         "Instalacao": r"Instalação:\s+(\d+)",
-#         "Endereço, N°": r"Endereço:\s+((?!STO ANTONIO , 0)[^\n]+)(?=\s+Bairro|$)",
-#         "Bairro": r"Bairro:\s+([A-Za-z\s]+)(?=\s+Instalação|$)",
-#         "Complemento": r"Complemento:\s+([^\n]+)(?=\s+Fatura|$)",
-#         "N° Fatura": r"Fatura:\s+(\d+)",
-#         "Classe Principal": r"Classe Principal\s*[^0-9]*\s*(\d+)",
-#         "Classe de Consumo": r"Classe de Consumo(?:.*?\n)*?.*?(?:\d+\n)(\d+)",
-#         "Tensão": r"(?<=\bTensão\b)(?:\D*\d+){2}\s*(\S+)",
-#         "Fase": r"(?<=\bFase\b)(?:\D*\d+){3}\D*(\S+)",
-#         "Data Fatura": r"(?<=\bData Fat\b)(?:\D*\d+){5}\D*(\S+)",
-#         "Dias Fatura": r"(?<=\bDias Fat\b)(?:\D*\d+){8}\D*(\S+)",
-#         "Data Leitura Anterior": r"(?<=\bDta\.Leit\.Ant\b|\bDat\.Leit\.Ant\b)(?:\D*\d+){9}\D*(\S+)",
-#         "Data Leitura Atual": r"(?<=\bDta\.Leit\.Ant\b|\bDat\.Leit\.Ant\b)(?:\D*\d+){12}\D*(\S+)",
-        
-#         # "Reaviso": r"(?<=\bReaviso\b)",
-#         # "Corte": r"(?<=\bDta\.Leit\.Ant\b|\bDat\.Leit\.Ant\b)(?:\D*\d+){12}\D*(\S+)",
-        
-#         "V.T.: Icms(BaseCalculo)": r"ICMS\s+(?:\D*\d+.+?\d+\n){0}([\d,\.]+)",
-#         "V.T.: Icms(Aliquota)": r"ICMS\s+(?:\D*\d+.+?\d+\n){1}([\d,\.]+)",
-#         "V.T.: Icms(Valor)": r"ICMS\s+(?:\D*\d+.+?\d+\n){2}([\d,\.]+)",
-#         "V.T.: Cofins": r"COFINS\s+(?:\D*\d+.+?\d+\n){0}([\d,\.]+)",
-#         "V.T.: Cofins(Aliquota)": r"COFINS\s+(?:\D*\d+.+?\d+\n){1}([\d,\.]+)",
-#         "V.T.: Cofins(Valor)": r"COFINS\s+(?:\D*\d+.+?\d+\n){2}([\d,\.]+)",
-#         "V.T.: Pis": r"PIS\s+(?:\D*\d+.+?\d+\n){0}([\d,\.]+)",
-#         "V.T.: Pis(Aliquota)": r"PIS\s+(?:\D*\d+.+?\d+\n){1}([\d,\.]+)",
-#         "V.T.: Pis(Valor)": r"PIS\s+(?:\D*\d+.+?\d+\n){2}([\d,\.]+)",
-        
-#         # "V.M.: Esp": r"(\bCAT\b)([A-Z])?\b",
-#         "V.M.: Medidor": r"CAT\s+(?:\D*\d+.+?\d+\n){0}([\d,\.]+)",
-#         # "V.M.: CTE": r"CAT\s+(?:\D*\d+.+?\d+\n){1}([\d,\.]+)",
-#         # "V.M.: FP": r"CAT\s+(?:\D*\d+.+?\d+\n){2}([\d,\.]+)",
-#         # "V.M.: Leitura_Anterior": r"CAT\s+(?:\D*\d+.+?\d+\n){3}([\d,\.]+)",
-#         # "V.M.: Leitura_Atual": r"CAT\s+(?:\D*\d+.+?\d+\n){4}([\d,\.]+)",
-#         # "V.M.: Medido": r"CAT\s+(?:\D*\d+.+?\d+\n){5}([\d,\.]+)",
-#         # "V.M.: Faturado": r"CAT\s+(?:\D*\d+.+?\d+\n){6}([\d,\.]+)",
-        
-#         # "V.F.: Consumo(Quantidade)": r"Consumo\s+(?:)([\d.,]+)[\s\S]*?",
-#         # "V.F.: Consumo(Preço)": r"Consumo\s+(?:)([\d.,]+)[\s\S]*?",
-#         # "V.F.: Consumo(Valor)": r"Consumo\s+(?:)([\d.,]+)[\s\S]*?",
-#         # "Cip-Ilum Pub Pref Munic (Valor)":r"Cip-Ilum Pub Pref Munic\s+([\d\.,]+)",
-#         # "V.F.: Adcional Bandeira (Valor)": r"Adicional Bandeira\s+([\d\.,]+)",
-#         # "V.F.: Crédito Prazo Atendimento (Valor)": r"Crédito Prazo Atendimento\s+([\d\.,]+)",
-#         # "V.F.: Tributo a Reter IRPJ (Valor) ": r"Tributo a Reter IRPJ\s+([\d\.,]+)",
-#         # "V.F.: Saldo em Aberto (Valor)": r"Saldo em aberto\s+([\d\.,]+)",
-#     }, start_page=1, max_pages=2, pages_to_extract=None)
+def main(list_input_files=None, references_dict=None, name_output_file="ArquivosExtraidos.xlsx", pages_to_extract=None, max_pages=None, start_page=0):
+    try:
+        # Extração de dados do PDF
+        extracted_data = extract_references_from_pdfs(
+            list_input_files, references_dict, start_page=start_page, max_pages=max_pages, pages_to_extract=pages_to_extract
+        )
+    except Exception as e:
+        logging.error(f"Erro ao extrair referências dos PDFs: {e}")
+        return None  # Ou um valor padrão
+
+    try:
+        # Preparação da tabela
+        df = create_table(extracted_data, references_dict)
+    except Exception as e:
+        logging.error(f"Erro ao criar a tabela: {e}")
+        return None  # Ou um valor padrão
+
+    try:
+        # Salvar os dados no Excel
+        return save_table_to_file(df, name_output_file)
+    except Exception as e:
+        logging.error(f"Erro ao salvar a tabela no arquivo Excel: {e}")
+        return None  # Ou um valor padrão
